b2a0638c73591e400d20e1d35922a44a114fb3b7
Brennan Heisler <learn@heislercreative.com>
2018-03-23 22:23:23 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -25,7 +25,7 @@ class LandmarksController < ApplicationController
   end
 
   patch '/landmarks' do
-    raise params.inspect
+    binding.pry
     @landmark = Landmark.find_by_id(params[:id])
     @landmark.update(params[:landmark])
     redirect to "/landmarks/#{@landmark.id}"
