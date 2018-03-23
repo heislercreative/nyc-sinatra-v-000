64f37b62c31c9acd1b6a766c4caca1f4f909b61b
Brennan Heisler <learn@heislercreative.com>
2018-03-23 22:07:44 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -19,4 +19,9 @@ class LandmarksController < ApplicationController
     erb :'/landmarks/show'
   end
 
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/edit'
+  end
+
 end
